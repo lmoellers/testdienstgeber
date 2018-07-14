d5ce39762778bc3dd289f6f3016032e619d9c743
lmoellers <laura_kristin.moellers@smail.th-koeln.de>
2018-07-14 23:59:33 +0200

    Änderungen

--- Dienstgeber/dienstgeber.js
+++ Dienstgeber/dienstgeber.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var redis = require('redis');
 var client = redis.createClient();
+
 // nur zum debug
 var util = require('util');
 
@@ -18,16 +19,11 @@ app.use('/Bewertung', require('./routes/bewertung_ressource'));
 app.use('/Notepad', require('./routes/notepad_ressource'));
 app.use('/',require('./routes/servicedokument_ressource'));
 
-app.get('/', function (req, res){
-  response.render('index', {
-    title: 'Tradinggameapplication'
-  });
-});
 
-client.on('error', function (err) {
-    console.log(err);
-    process.exit(1);
-});
+// client.on('error', function (err) {
+//     console.log(err);
+//     process.exit(1);
+// });
 
 // Start the server
 app.listen(app.get('port'), function () {
