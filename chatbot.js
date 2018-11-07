3ca7be030829286c399b4ad688cf529c7520a5f0
lmoellers <laura_kristin.moellers@smail.th-koeln.de>
2018-11-07 19:31:57 +0100

    chatbot1 test

--- chatbot.js
+++ chatbot.js
@@ -2,10 +2,13 @@ var readline = require('readline');
 var keypress = require('keypress');
 
 var questionNum = 0;													// keep count of question, used for IF condition.
-var question = printf("Hallo, wie heißt du?");				  // first question
+//var question = console.log("Hallo, wie heißt du?");				  // first question
 
 //var output = document.getElementById('output');				// store id="output" in output variable
 //output.innerHTML = question;													// ouput first question
+
+var stdin = process.openStdin();
+
 
 var rl = readline.createInterface({
   input: process.stdin,
@@ -13,12 +16,21 @@ var rl = readline.createInterface({
 });
 
 console.log('Hallo, du :)');
+console.log('Wie heißt du?');
+
+stdin.on('keypress', function(e){
+  //process.stdout.write('Bitte Enter drücken \n');
+  if (e.which == 13){
+    bot();
+    questionNum++;
+    process.exit();
+  }
+});
 
 function bot() {
-
+  var answer = input;
   if (questionNum == 0) {
     rl.question('Wie heißt du?', (answer) => {
-      var answer = input
       console.log('Hallo' + input + '!');
       //setTimeout(timedQuestion, 2000);
       rl.close();
@@ -27,7 +39,7 @@ function bot() {
 
   else if (questionNum == 1) {
       rl.question('Wie alt bist du?', (answer) => {
-        var answer = input
+        var answer = input;
         //setTimeout(timedQuestion, 2000);
         console.log('Das heißt du bist' + (2018 - input) + 'geboren worden.');
         rl.close();
@@ -35,7 +47,7 @@ function bot() {
     }
   else if (questionNum == 1) {
     rl.question('Wo wohnst du?', (answer) => {
-      var answer = input
+      var answer = input;
       console.log(input + 'liegt in Deutschland.')
     });
     //setTimeout(timedQuestion, 2000);
@@ -46,10 +58,11 @@ function bot() {
 //     output.innerHTML = question;
 // }
 
+
 //push enter key (using jquery), to run bot function.
-$("input")keypress(function(e) {
-  if (e.which == 13) {
-    bot();																						// run bot function when enter key pressed
-    questionNum++;																		// increase questionNum count by 1
-  }
-});
+// $(question).keypress(function(e) {
+//   if (e.which == 13) {
+//     bot();																						// run bot function when enter key pressed
+//     questionNum++;																		// increase questionNum count by 1
+//   }
+// });
